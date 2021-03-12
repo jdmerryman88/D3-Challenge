b44e41f3001163b28767a6b7dea8f1766c767cf4
Justin Merryman <justinmerryman@yahoo.com>
2021-03-12 16:19:23 -0600

    changed cirlce size

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -56,7 +56,7 @@ let circlesGroup = chartGroup.selectAll("circle")
     .append("circle")
     .attr("cx", (d) => xScale(d.poverty))
     .attr("cy", d => yScale(d.healthcare))
-    .attr("r", "40")
+    .attr("r", "30")
     .classed("stateCircle" , true)
 
 chartGroup.selectAll(".text")
